0c142140277147c76e5f6c7db5a368a8ea519b37
tush87 <tusharj8788@gmail.com>
2023-03-27 15:58:17 +0530

    add home controller and basic setup of views

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressLayout = require('express-ejs-layouts');
+const port = 8000;
+
+const app = express();
+
+app.set("view engine","ejs");
+app.set('views','./views');
+app.set("layout extractScripts", true)
+app.set("layout extractStyles", true)
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static('./assets'));
+
+app.use(expressLayout);
+
+
+app.use('/',require('./routes/index.js'));
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("Server is up on port : ",port);
+})
