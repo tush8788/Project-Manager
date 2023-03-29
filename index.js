91cfa0d095d7cbf556fba1d9e45c7c35ea7c32df
tush87 <tusharj8788@gmail.com>
2023-03-29 20:31:37 +0530

    add mongostore for storing session

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const db = require('./config/mongoose');
 const passport = require('passport');
 const localStrategy = require('./config/passport-local-strategy');
 const session = require('express-session');
+const mongoStore = require('connect-mongo');
 const port = 8000;
 
 const app = express();
@@ -27,7 +28,13 @@ app.use(session({
     saveUninitialized:false,
     cookie:{
         maxAge:10000*10*60
-    }
+    },
+    store:mongoStore.create({
+        mongoUrl:process.env.MONGO_URL||"mongodb://localhost/UrlShortner",
+        autoRemove:false
+    },function(err){
+        console.log(err);
+    })
 }));
 
 app.use(passport.initialize());
