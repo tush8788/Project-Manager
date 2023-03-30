fdcbe7a9699c8871e5ec0be8dbab747e2f868d8a
tush87 <tusharj8788@gmail.com>
2023-03-30 10:38:04 +0530

    add style to header

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const passport = require('passport');
 const homeController = require('../controller/home_controller');
 
-router.get('/',passport.checkAuthentication,homeController.home);
+router.get('/',homeController.home);
 
-router.post('/create-url',homeController.Url);
+router.post('/create-url',passport.checkAuthentication,homeController.Url);
 
-router.get('/:url',homeController.verifyUrl);
+router.get('/:url',passport.checkAuthentication,homeController.verifyUrl);
 
-router.get('/delete/:id',homeController.deleteUrl);
+router.get('/delete/:id',passport.checkAuthentication,homeController.deleteUrl);
 
-router.get('/update/:id',homeController.updateUrlPage);
+router.get('/update/:id',passport.checkAuthentication,homeController.updateUrlPage);
 
-router.post('/update',homeController.updateUrl);
+router.post('/update',passport.checkAuthentication,homeController.updateUrl);
 
 router.use('/users',require('./user'));
 
